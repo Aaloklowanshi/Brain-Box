f3d44bc67dc895c9439132797980ac54b36c8f76
Aaloklowanshi <lowanshiaalok@gmail.com>
2024-06-28 14:18:10 +0530

    merged changes animated extra section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       {/* <KeyFeaturesSection/> */}
-      <TutorsSection />
+      {/* <TutorsSection /> */}
       <ExtraSection/>
       <TestimonialsSection />
 
