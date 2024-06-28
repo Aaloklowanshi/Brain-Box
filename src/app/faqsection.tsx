880fbad9bd6b4f290113d4b8bcef9f42c2d161ec
Gazal Bandil <gazalbandil2003@gmail.com>
2024-06-29 00:14:53 +0530

    website completed fuck off

--- src/app/faqsection.tsx
+++ src/app/faqsection.tsx
@@ -8,11 +8,15 @@ import {
 
 const Faqsection: React.FC = () => {
   return (
-    <div className="w-full px-4 py-8 md:py-16 lg:py-24 xl:py-20">
-      <div className="container mx-auto max-w-4xl">
+    <div className="w-full px-4 bg-gray-100 py-8 md:py-16 lg:py-24 xl:py-20 ">
+      <div>
+        <h1 className="text-3xl  font-bold justify-center text-center items-center">Customer FAQ - Your Questions Answered</h1>
+        <p className="text-md text-gray-400 justify-center text-center items-center mt-[2%]">make your all questions answered</p>
+      </div>
+      <div className="container mt-[5%] mx-auto max-w-4xl">
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
-            <AccordionTrigger>How are the classes conducted ? </AccordionTrigger>
+            <AccordionTrigger >How are the classes conducted ? </AccordionTrigger>
             <AccordionContent>
             They are conducted one on one via zoom.
             </AccordionContent>
