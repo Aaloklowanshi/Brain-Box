b2330dbb3bed2259442ea1b4cc9df99ec169dd99
Gazal Bandil <gazalbandil2003@gmail.com>
2024-06-30 14:04:50 +0530

    final codee

--- src/app/contactsection.tsx
+++ src/app/contactsection.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import React from "react";
-import Contact from "../assets/Mention (1).gif"
-import bg from "../assets/background.png"
-import Image from "next/image";
+
+
 import { sendEmail } from "@/actions/sendemail";
 import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
@@ -19,17 +18,17 @@ const ContactSection: React.FC = () => {
     <section 
     id="contact"
     className="w-full py-12 md:py-24 lg:py-32 bg-slate-50">
-      <div className="p-[2%] bg-[#ff6347f4]">
-       <h2 className="text-white font-semibold text-left text-3xl ms-[3%]">Subscribe now to make learning easy and smart!</h2>
+      <div className="py-[2%] bg-[#ff6347f4]">
+       <h2 className="text-xl text-white font-semibold text-left sm:text-3xl ms-[3%] md:text-3xl">Subscribe now to make learning easy and smart!</h2>
 </div>
-      <div className="flex flex-row container px-4 md:px-6">
+      <div className="flex flex-col justify-center items-center  container px-4 sm:flex-row  md:px-6  ">
         <div className="flex flex-col w-[35%] items-center justify-center space-y-4 text-center animate__animated animate__fadeIn">
           <div className="space-y-4">
             <div className="inline-block rounded-lg  bg-gray-100 px-3 py-1 text-md dark:bg-gray-800">
               Contact Us
             </div>
-            <h2 className="text-3xl font-bold tracking-tighter text-[#1a202c] sm:text-5xl">
-              Get in <span className="text-[#ff6347]">Touch</span>
+            <h2 className=" text-4xl font-bold tracking-tighter text-[#1a202c] sm:text-5xl">
+              Get in <span className="text-[#ff6347]"> Touch</span>
             </h2>
             <p className="mt-[2%] max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
               Have any questions or need further information? Contact us today!
@@ -49,12 +48,12 @@ const ContactSection: React.FC = () => {
         <br></br>
         <br></br>
         <div className="mx-auto w-[70%] h-[60%] justify-center items-center mt-[5%] bg-[#ffffff] max-w-2xl py-12 animate__animated animate__fadeInUp p-[5%] rounded-md shadow-md" 
-        //  style={{ 
-        //    backgroundImage:'url("/background.png")',
-        //    backgroundSize: 'cover',
-        //    backgroundPosition: 'center',
-        //    backgroundRepeat: 'no-repeat'
-        //  }}
+         style={{ 
+           backgroundImage:'url("/background.png")',
+           backgroundSize: 'cover',
+           backgroundPosition: 'center',
+           backgroundRepeat: 'no-repeat'
+         }}
         >
           <form 
           className="grid gap-4"
